21cc204783faced1ac137dca9b597323326f7683
VINEETH M <vineethm1627@gmail.com>
2019-11-10 04:17:05 +0530

    added the final weight based algorithm

--- app.py
+++ app.py
@@ -114,3 +114,93 @@ def viewreports():
 
 
 app.run(debug = True)
+
+def result(dict1, dict2):
+    weights = [2, 0, 2, 2, 3, 4, 3, 4, 4, 5]
+    polarity_score = []
+    subjectivity_score = 0
+
+    for j in range(len(dict2[1])):
+        polarity_score.append(dict2[1][j][0])
+        subjectivity_score+=dict2[1][j][1]
+
+    total_score=[a*b for a,b in zip(polarity_score,weights)]
+    text_score=sum(total_score)/len(total_score)
+    subjectivity_score=subjectivity_score/len(total_score)
+    print("POLARITY SCORE",text_score)
+    empath_dict={}
+    
+    for i in range(len(dict2[0])):
+        for j in dict2[0][i]:
+            if(j in empath_dict):
+                empath_dict[j]+=dict2[0][i][j]
+            else:
+                empath_dict[j]=dict2[0][i][j]
+
+    for i in empath_dict:
+        if(i not in ['cheerfulness','pride','celebration','heroic','optmism']):
+            empath_dict[i]*=-1
+
+    empath_score=0
+     
+    for i in empath_dict:
+         empath_score+=empath_dict[i]
+         
+
+    empath_score = empath_score/len(dict2[0])
+    empath_score = empath_score/10
+
+    print("EMPATH SCORE",empath_score)
+
+    dict11={}
+    for i in dict1[0]:
+        print("running----------------------------------------")
+        if(i=="angry"):
+            dict11[i]=(dict1[0][i]*(-0.4))/dict1[1]
+        elif(i=="happy"):
+            dict11[i]=(dict1[0][i]*(1))/dict1[1]
+        elif(i=="neutral"):
+            dict11[i]=(dict1[0][i]*(0))/dict1[1]
+        elif(i=="disgust"):
+            dict11[i]=(dict1[0][i]*(0.1))/dict1[1] 
+        elif(i=="surprised"):
+            dict11[i]=(dict1[0][i]*(0.2))/dict1[1]
+        elif(i=="sad"):
+            dict11[i]=(dict1[0][i]*(-1))/dict1[1]
+        else:
+            dict11[i]=(dict1[0][i]*(-0.6))/dict1[1]
+    print(dict11)
+    print(dict1[1])
+    video_score=0
+    for i in dict11:
+        video_score+=dict11[i]
+    video_score=video_score/7
+    print("VIDEO SCORE",video_score)
+    total_final_score=(0.6*text_score)+(0.15*video_score)+(0.25*empath_score)
+    
+    gen=dict2[2];
+    pause=sum(dict2[3])/len(dict2[3]);
+    ros=sum(dict2[4])/len(dict2[4]);
+    strr=dict2[5];
+    str1="good"
+
+    print("TOTAL SCORE:",total_final_score)
+    print('GENDER ',dict2[2])
+    print('PAUSES',sum(dict2[3])/len(dict2[3])) 
+    print('RATE OF SPEECH',sum(dict2[4])/len(dict2[4]))  
+    print('SPEAKING TIME RATIO',dict2[5]) 
+    print("SUBJECTIVITY SCORE",subjectivity_score)
+
+    if(total_final_score<-0.5):
+        str1 = "Significantly distressed. Needs some improvement. Try to stay calm. "
+        print(str1)
+    elif(total_final_score>=-0.5 and total_final_score<0):
+        str1 = "Moderately distressed. Freshen up your mind with positive thoughts. "
+        print(str1)
+    elif(total_final_score>=0 and total_final_score<0.5):
+        str1 = "Stable state of mind. Cheer up yourself"
+        print(str1)   
+    else:
+        str1 = "Well balanced state of mind. Cheer up yourself."
+        print(str1)     
+     
